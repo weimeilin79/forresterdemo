8d1a2637c59cd55ddd40a4ea6923eed4956367ec
weimeilin79 <weimeilin@gmail.com>
2016-08-31 11:10:20 +0800

    Add Kafka sender

--- MessageSender/src/main/java/com/mycompany/camel/producer/MyProcessor.java
+++ MessageSender/src/main/java/com/mycompany/camel/producer/MyProcessor.java
@@ -7,7 +7,7 @@ import org.apache.camel.component.kafka.KafkaConstants;
 public class MyProcessor implements Processor {
 	  public void process(Exchange exchange) throws Exception {
 		  
-		  exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY, 1);
-          exchange.getIn().setHeader(KafkaConstants.KEY, "1");
+		  exchange.getIn().setHeader(KafkaConstants.PARTITION_KEY, 0);
+          exchange.getIn().setHeader(KafkaConstants.KEY, "0");
 	  }
 }
